743966579b5c87d9a1f8b516bad028840b1efb1e
viv-ops <mjfdsfhxfyjjnxbm@gmail.com>
2025-05-22 18:53:45 +0500

    text

---
+++ script.js
@@ -0,0 +1,6 @@
+// 2. Используйте console.log для вывода строки "Hello, World!" в консоль браузера.
+console.log("Hello, world!");
+
+/* 3. Вызов функции `alert`
+   4. Команда alert используется в JavaScript для вывода всплывающего окна*/
+alert("Hello");
